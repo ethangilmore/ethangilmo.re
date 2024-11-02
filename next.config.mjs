63ddd88ca30f6bc4c8511e77b15a46a3a8727bcb
Ethan Gilmore <90002503+ethangilmore@users.noreply.github.com>
2024-11-02 12:07:55 -0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,11 @@ const withMDX = nextMDX();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: "/ethangilmo.re",
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
-}
+  images: {
+    unoptimized: true,
+  },
+};
  
 export default withMDX(nextConfig)
